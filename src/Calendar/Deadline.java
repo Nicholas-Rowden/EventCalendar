320191eef289845f759519bbcd9333ddfd1f51aa
Hello World Assignment <nicholasarowden@gmail.com>
2025-02-09 17:03:10 -0600

    -Adds the event deadline class

--- src/Calendar/Deadline.java
+++ src/Calendar/Deadline.java
@@ -1,4 +1,27 @@
 package Calendar;
 
-public class Deadline {
+import java.time.LocalDateTime;
+
+public class Deadline extends Event implements Completable {
+    private boolean complete;
+
+    public Deadline(String name, LocalDateTime deadline) {
+        super(name, deadline);
+        this.complete = false;
+    }
+
+    @Override
+    public void complete() {
+        this.complete = true;
+    }
+
+    @Override
+    public boolean isComplete() {
+        return complete;
+    }
+
+    @Override
+    public String getName() {  // Proper implementation
+        return super.getEventName();  // Use the protected method from Event
+    }
 }
